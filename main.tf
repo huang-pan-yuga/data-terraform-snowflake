d22fb430bfd09cce587f90c01008803f1d0f49c6
Huang Pan <huang.pan@yugalabs.io>
2024-02-06 17:21:38 -0800

    added snowflake user terraform

--- main.tf
+++ main.tf
@@ -39,14 +39,14 @@ resource "snowflake_role" "hex" {
 
 # https://registry.terraform.io/providers/Snowflake-Labs/snowflake/latest/docs/resources/user
 # https://docs.snowflake.com/en/sql-reference/sql/create-user for default values
-/*resource "snowflake_user" "terraform" {
+resource "snowflake_user" "terraform" {
   name                  = "TERRAFORM"
   comment               = "For Terraform integration"
   password              = "secret"
   email                 = "data.admin@yugalabs.io"
   default_warehouse     = "COMPUTE_WH"
   default_role          = "TERRAFORM_ROLE"
-}*/
+}
 
 resource "snowflake_user" "accelbyte" {
   name                  = "ACCELBYTE"
@@ -58,7 +58,8 @@ resource "snowflake_user" "accelbyte" {
   default_namespace     = "TELEMETRY_DB.BRONZE_ACCELBYTE"
   # For AccelByte Snowflake Connector
   # https://docs.google.com/spreadsheets/d/1XZGH6kVMvaQmz-1XIZZHHXPMaVfmH8c0KyGYDk8uZXI/edit?usp=sharing
-  rsa_public_key        = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3lhqdqy7phd96KYZvn5f6d/Ry1du1buyhyM3yYjwE+tsm5hIXAqfm1sqjaU2jjQalEsxMTCJBB4SNG3Fxw9Q2bi9vd+2rGt3NQmMSP3pE7UncDo0lPZwmhRufhZ1dD4/nFM7xkFwm/hf2KBIA2XBjaF+gIS44QnLoUKscruYaZsk2f0QTVeEgoUcx2ddNOOJuXp1MmQovu3eBWpyFGpqi4pYRR4kfAdQroQByI+90PMFh2tFUpTqa6AKJFEViwR68sKJCfs5t3Mtwy25+VkSNmwEP574MJVQjlW0Uje7TL14XpdEvYEotH5wOA2MM7opEka480eknpmjkUSMt6M18QIDAQAB"
+  #rsa_public_key        = ""
+  #rsa_public_key_2      = ""
 }
 
 resource "snowflake_user" "aws" {
